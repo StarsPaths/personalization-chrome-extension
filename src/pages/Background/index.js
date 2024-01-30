ae915b009472e067bac7d8077a50d9128b02fc6c
CBCzed <1376778365@qq.com>
2024-01-30 11:20:41 +0800

    chrome extenison for apz self service

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,49 +1,107 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-       chrome.storage.sync.get(['environments'], (result) => {
+// 当前标签页 url
+let currentTab = {};
+let requestBodies = {};
+let requestHeaders = {};
+let apzAdminPortalAuthToken = ''
+const BLOCKING_URLS = [
+  'http://localhost:9007/personalization/admin/graphql',
+  'https://bff-api.automizely.io/personalization/admin/graphql',
+  'https://bff-api.automizely.com/personalization/admin/graphql',
+];
+const APP_KEYS = []
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  currentTab = tab;
+});
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  apzAdminPortalAuthToken = request.authToken;
+})
+// 拦截请求体或请求地址，请求体和请求地址不可同时更改，只可改其中之一
+chrome.webRequest.onBeforeRequest.addListener(
+  function (details) {
+    requestBodies = details.requestBody;
+    const isProxy = getIsProxy(details.method, details.requestBody);
+    console.log('onBeforeRequest isProxy: ', isProxy);
+    if (isProxy) {
+      const redirectUrl = getRequestRedirectUrl(details.url);
+      return { redirectUrl: redirectUrl };
+    }
+    return { cancel: false };
+  },
+  { urls: BLOCKING_URLS },
+  ['blocking', 'requestBody']
+);
+/**
+ * 拦截请求头
+ * onBeforeSendHeaders 无法执行异步的操作，若是要添加异步的 token 则可能需要在业务测添加
+*/
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function (details) {
+    requestHeaders = details.requestHeaders;
+    const isProxy = getIsProxy(details.method, requestBodies);
+    let headers = details.requestHeaders;
+    if (isProxy) {
+      headers = headers.filter((item) => {
+        return item.name !== 'authorization';
+      });
+      headers.push({
+        name: 'authorization',
+        value: `${apzAdminPortalAuthToken?.type} ${apzAdminPortalAuthToken?.token}`,
+      });
+    }
+    return { requestHeaders: headers };
+  },
+  { urls: BLOCKING_URLS },
+  ['blocking', 'requestHeaders']
+);
 
-              const proxy={
-                incy:{
-                  source:'sdks.automizely.io',
-                  target:'release-incy-sdks.automizely.io'
-                },
-                kiwi:{
-                  source:'sdks.automizely.io',
-                  target:'release-kiwi-sdks.automizely.io'
-                },
-                staging:{
-                  source:'sdks.automizely.com',
-                  target:'staging-sdks.automizely.com'
-                }
-              };
-              const initialRules = [
-                {
-                  id: 2,
-                  priority: 2,
-                  action: {
-                    type:"redirect", 
-                    redirect: {
-                        transform: { scheme: "https", host:proxy[result?.environments]?.target }
-                    }
-                  },
-                  condition: {
-                  urlFilter:  proxy[result?.environments]?.source ,
-                },
-                },
-              ];
+/**
+ * 拦截条件
+ * 1. 从 APZ Admin Portal 来的
+ * 2. 发起请求的接口时的 GetWidgetById / UpdateWidgetSetting
+ * 3. 有权限的 app key
+*/
+function getIsProxy(requestMethod, requestBodies) {
+  const appkey = requestHeaders?.find(
+    (item) => item.name === 'am-app-key'
+  )?.value;
+  const hasPermissionAppKey = APP_KEYS.includes(appkey);
+  const isAPZAdminPortal = getUrlSource();
+  const isIntercepted = getInterceptedGraphql(requestMethod, requestBodies);
+  return isAPZAdminPortal && isIntercepted && !hasPermissionAppKey;
+}
 
-                 chrome.declarativeNetRequest.getDynamicRules(function (res) {
-                  console.log('remove');
-                   let rules = res.map((e) => e.id);
-                   chrome.declarativeNetRequest.updateDynamicRules(
-                     {
-                       addRules: result?.environments!=='none'? initialRules:[], //Rule[] optional
-                       removeRuleIds: rules, //number[] optional
-                     },
-                     function (callback) {}
-                   );
-                 });
-                 });
-       
-      // 发送响应消息
-       sendResponse({ response: "Message received successfully!" });
-     });
+function getUrlSource() {
+  const url = new URL(currentTab.url);
+  const searchParams = new URLSearchParams(url.search);
+  const source = searchParams.get('source');
+  return source === 'apz_admin_portal';
+}
+
+function getInterceptedGraphql(requestMethod, requestBodies) {
+  if (requestMethod === 'POST' && requestBodies) {
+    const decoder = new TextDecoder('utf-8');
+    const requestBody = JSON.parse(decoder.decode(requestBodies.raw[0].bytes));
+    const isArray = Array.isArray(requestBody);
+    if (isArray) {
+      const INTERCEPTED_GRAPHQL_OPERATIONS = ['GetWidgetById', 'UpdateWidgetSetting'];
+      const isIntercepted = INTERCEPTED_GRAPHQL_OPERATIONS.includes(
+        requestBody[0].operationName
+      );
+      return isIntercepted;
+    }
+    return false;
+  }
+  return false
+}
+
+function getRequestRedirectUrl(requestUrl) {
+  const REDIRECT_URL = new Map([
+    ['http://localhost:9007', 'http://localhost:9006'],
+    ['http://127.0.0.1:9007', 'http://localhost:9006'],
+    ['https://bff-api.automizely.io', 'https://bff-api.automizely.me'],
+    ['https://bff-api.automizely.com', 'https://bff-api.automizely.org'],
+  ]);
+  const url = new URL(requestUrl);
+  const redirectDomain = REDIRECT_URL.get(url.origin);
+  return redirectDomain + '/personalization/portal/graphql';
+}
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,3 +4,15 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+
+window.addEventListener(
+  'message',
+  function (event) {
+    // 我们只接受来自我们自己的消息
+    if (event.source != window) return;
+    if (event.data?.source === 'apz_admin_portal') {
+      chrome.runtime.sendMessage({ authToken: event.data });
+    }
+  },
+  false
+);
